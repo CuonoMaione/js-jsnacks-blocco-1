9361fa5927dada5746aedde0e3d72226648a3426
CuonoMaione <cuonomaione99@outlook.it>
2023-05-18 16:15:18 +0200

    jsnack2

---
+++ jsnack2/myscript.js
@@ -0,0 +1,20 @@
+const UsersList = ["Francesco" , "Cuono" , "Antonio" , "Anna" , "Sabrina"];
+
+let MyName = prompt("insert your name");
+
+for (let i = 0 ; i < UsersList.length ; i++) {
+    
+    if ( MyName == UsersList[i] ) {
+        Identification = true;
+    } else {
+        Identification = false;
+    }
+    
+    console.log(UsersList[i])
+    console.log(Identification);
+
+
+}
+
+
+
