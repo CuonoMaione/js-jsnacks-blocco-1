fb96b991a3759cde72fefa1eccf9dfafe5d6b8ef
CuonoMaione <cuonomaione99@outlook.it>
2023-05-18 15:51:02 +0200

    jsnack1

---
+++ jsnack1/myscript.js
@@ -0,0 +1,14 @@
+const Number1 = parseInt(prompt("insert a number"));
+const Number2 = parseInt(prompt("insert a number"));
+const Number3 = parseInt(prompt("insert a number"));
+const Number4 = parseInt(prompt("insert a number"));
+const Number5 = parseInt(prompt("insert a number"));
+const Number6 = parseInt(prompt("insert a number"));
+const Number7 = parseInt(prompt("insert a number"));
+const Number8 = parseInt(prompt("insert a number"));
+const Number9 = parseInt(prompt("insert a number"));
+const Number10 = parseInt(prompt("insert a number"));
+
+const Addition = (Number1 + Number2 + Number3 + Number4 + Number5 + Number6 + Number7 + Number8 + Number9 + Number10 )
+
+console.log(Addition);
