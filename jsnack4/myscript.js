0338907d40aa7bdb3ab1e6afa1199980197a40f8
CuonoMaione <cuonomaione99@outlook.it>
2023-05-18 17:27:45 +0200

    jsnack4

---
+++ jsnack4/myscript.js
@@ -0,0 +1,16 @@
+const Numbers = prompt("insert number with 4 digit.");
+console.log(Numbers);
+
+let Sum = parseInt(Numbers.charAt(0)) + parseInt(Numbers.charAt(1)) + parseInt(Numbers.charAt(2)) + parseInt(Numbers.charAt(3)) ;
+
+console.log(Sum)
+
+
+/*
+for (let i = 0; i < Numbers.length; i++) {
+    
+    let digit = parseInt((Numbers.charAt(i)));
+    
+} 
+
+*/
